4a05e434f9aa0f3ca5d233585e69bb2baa5770d1
David Zapata <luisdavid@google.com>
2022-08-31 11:37:10 -0500

    About me section Done

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Header
 const headerURL = 'templates/header.html'
+let navBarLink;
 fetch(headerURL)
     .then(res => res.text())
     .then(data => fetchHeader(data));
@@ -12,6 +13,7 @@ function fetchHeader(data) {
             document.querySelector('.header_nav').classList.toggle('hide')
         })
     }
+    setActiveLink();
 }
 const footerURL = 'templates/footer.html'
 fetch(footerURL)
@@ -27,27 +29,46 @@ const expandArrow = '<i class="fa-solid fa-angle-up"></i>';
 const collapseArrow = '<i class="fa-solid fa-angle-down"></i>';
 const carouselTitle = document.querySelectorAll('.faq__accordion-wrapper--title');
 
-document.addEventListener('DOMContentLoaded', () =>{
+document.addEventListener('DOMContentLoaded', () => {
     carouselTitle[0].click()
 })
 
-carouselTitle.forEach( title => {
+carouselTitle.forEach(title => {
     title.addEventListener('click', e => {
-try {
-    if(e.target.parentElement.children[2].classList.contains('hide')){
-        e.target.parentElement.children[2].classList.remove('hide')
-        e.target.children[1].innerHTML = expandArrow;
-    }else{
-        e.target.parentElement.children[2].classList.add('hide')
-        e.target.children[1].innerHTML = collapseArrow;
+        try {
+            if (e.target.parentElement.children[2].classList.contains('hide')) {
+                e.target.parentElement.children[2].classList.remove('hide')
+                e.target.children[1].innerHTML = expandArrow;
+            } else {
+                e.target.parentElement.children[2].classList.add('hide')
+                e.target.children[1].innerHTML = collapseArrow;
 
-    }
-} catch (error) {
-    e.target.parentElement.click()
-}
-        
+            }
+        } catch (error) {
+            e.target.parentElement.click()
+        }
+
     })
-})
+});
+
+//Set active link
+
+//active-link
+
+
+function setActiveLink() {
+    const navBarLink = document.querySelectorAll('a.navbar-link')
+    const headTitle = document.querySelectorAll('title');
+    console.log(headTitle[0].textContent.split(' | ')[1]);
+
+    console.log(navBarLink)
+
+    navBarLink.forEach(link => {
+        if (headTitle[0].textContent.split(' | ')[1] == link.textContent) {
+            link.classList.add('active-link')
+        }
+    })
+}
 
 
 // const arrowButtons = document.querySelectorAll('.faq__accordion-wrapper--title > span');
